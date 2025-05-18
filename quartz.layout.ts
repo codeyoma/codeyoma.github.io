6fb04e54f253cc869495bca7c272f14649cab22a
yoma <codeyoma@gmail.com>
2025-05-18 11:04:48 +0900

    Quartz sync: May 18, 2025, 11:04 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -13,6 +13,7 @@ export const sharedPageComponents: SharedLayout = {
                 repoId: 'R_kgDOOraRoA',
                 category: 'General',
                 categoryId: 'DIC_kwDOOraRoM4CqR8o',
+                // loading: 'lazy',
             }
         }),
     ],
@@ -21,7 +22,6 @@ export const sharedPageComponents: SharedLayout = {
             "Blog source": "https://github.com/codeyoma/codeyoma.github.io",
             GitHub: "https://github.com/codeyoma",
             LinkedIn: "https://www.linkedin.com/in/codeyoma",
-            // "Discord Community": "https://discord.gg/cRFFHYye7t",
         },
     }),
 }
